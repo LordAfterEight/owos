482a0df5eb9fe6adf59d08109de36993016eaac5
LordAfterEight <104775671+LordAfterEight@users.noreply.github.com>
2026-07-03 07:33:34 +0000

    Slight improvements

--- src/apps/filesystem.rs
+++ src/apps/filesystem.rs
@@ -50,9 +50,34 @@ impl crate::proc::Process for OfsDriver {
     fn on_tick(&mut self) -> Result<crate::proc::ProcessEvent, crate::proc::ProcessError> {
         if self.closing {
             for _ in 0..1000 {
-                if self.files.pop().is_none() { break; }
+                if self.files.pop().is_none() {
+                    break;
+                }
             }
             if self.files.is_empty() {
+                let fb = crate::kui::kdraw::GLOBAL_FB.get().unwrap().0;
+                let text_length = crate::kui::kdraw::text_length(
+                    "Deallocating Files...",
+                    &crate::kui::kfont::ORBITRON_REGULAR,
+                    20.0,
+                );
+                let x_pos = fb.width as u32 / 2 - text_length as u32 / 2;
+                crate::kui::kdraw::draw_rect(
+                    x_pos - 10,
+                    fb.height as u32 / 2,
+                    text_length as u32 + 10,
+                    30,
+                    15,
+                    0xF3E600
+                );
+                crate::kui::kdraw::draw_text(
+                    x_pos,
+                    fb.height as u32 / 2 + 8,
+                    20.0,
+                    &crate::kui::kfont::ORBITRON_REGULAR,
+                    "Deallocating Files",
+                    0x0
+                );
                 return Ok(crate::proc::ProcessEvent::Closed(0));
             }
             return Ok(crate::proc::ProcessEvent::Yielded);
@@ -60,13 +85,13 @@ impl crate::proc::Process for OfsDriver {
 
         self.ticks += 1;
 
-        if self.ticks % 10 == 0 {
+        if self.ticks % 5 == 0 {
             let mut file = crate::ofs::PlaintextFile::new("TestFile.txt").unwrap();
             file.write_bytes("Hello World!".as_bytes()).unwrap();
             self.files.push(file);
         }
 
-        if self.files.len() == 16_700_000 {
+        if self.files.len() == 15_000_000 {
             self.closing = true;
         }
 
@@ -89,7 +114,6 @@ impl crate::proc::Process for OfsDriver {
                 0x55EAD4,
             );
         }
-
 
         Ok(crate::proc::ProcessEvent::Yielded)
     }
